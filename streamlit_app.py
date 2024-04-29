3c50d89713edf5a67952b3a06c21a51ad2d33e2e
Yroá Robledo Ferreira <149791165+yroarf@users.noreply.github.com>
2024-04-29 15:49:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ mostraTexto = st.selectbox('Mostrar Texto', options=['Sim', 'Não', 'Talvez'], k
 
 if st.session_state.mostraTexto == 'Sim':
     st.write(' Teste de deploy usando streamlit: OK!')
-elif st.session_state.motraTexto == 'Não':
+elif st.session_state.mostraTexto == 'Não':
     dfAreaPrest_0 = pd.read_csv("df_Mun_UF_Area.csv", dtype=str)
     st.dataframe(dfAreaPrest_0)
 else:
